fcd34c5d17b71dbc888c2d034d677c6f975b984e
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-05-07 02:12:09 -0500

    add an expression to see the object that is send to the DOM to be rendered, in this case out app

--- bookshelf/src/index.js
+++ bookshelf/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (<App />);
+// console.log(app);
+
+ReactDOM.render( app, document.getElementById('root'));
 registerServiceWorker();
