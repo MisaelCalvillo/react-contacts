864062d54b3491cb204a448684f0283263a1f1c8
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-05-09 13:51:14 -0500

    Change the functionality of changing to 'create contact' to a Link

--- bookshelf/src/index.js
+++ bookshelf/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const app = (<App />);
+const app = (
+<BrowserRouter>
+<App />
+</BrowserRouter>);
 // console.log(app);
 
 ReactDOM.render( app, document.getElementById('root'));
